bac6f92f6cc30771e461f390cb9676d22fc7616d
AngelDv18 <133921931+AngelDv18@users.noreply.github.com>
2023-06-26 15:54:12 -0500

    cambiado a try catch

--- Aplicacion de tienda 2.0.2/Entity/Menú.cs
+++ Aplicacion de tienda 2.0.2/Entity/Menú.cs
@@ -15,39 +15,85 @@ namespace Aplicacion_de_tienda_2._0._2.Entity
             funciones = new Funciones();
             cliente = new Cliente();
         }
-        public void MostrarMenu()
+        public double MostrarMenu()
         {
-            Console.WriteLine("Bienvenido al programa de compras");
-            Console.WriteLine("---------------------------------");
-            Console.Write("Ingresa tu nombre: ");
-            cliente.Nombre = Console.ReadLine();
-            Console.Write("Ingresa tu apellido: ");
-            cliente.Apellido = Console.ReadLine();
-            Console.Write("Ingresa tu edad: ");
-            cliente.Edad = int.Parse(Console.ReadLine());
-            Console.Write("Ingresa tu dirección: ");
-            cliente.Direccion = Console.ReadLine();
-            Console.Clear();
-            if (cliente.Edad >= 18)
+            try
             {
-                Console.Clear();
-                Console.Write("Ingresa el total de compra: ");
-                double totalCompra = int.Parse(Console.ReadLine());
-                Console.Clear();
-                Console.WriteLine("--------------Datos del Cliente.-------------------");
-                Console.WriteLine($"Cliente: {cliente.Nombre} {cliente.Apellido}");
-                Console.WriteLine($"Edad: {cliente.Edad}");
-                Console.WriteLine($"Dirección: {cliente.Direccion}");
+                Console.WriteLine("Bienvenido al programa de compras");
                 Console.WriteLine("---------------------------------");
-                funciones.CalcularDescuento(totalCompra);
+                Console.Write("Ingresa tu nombre: ");
+                cliente.Nombre = Console.ReadLine();
+                Console.Write("Ingresa tu apellido: ");
+                cliente.Apellido = Console.ReadLine();
+                Console.Write("Ingresa tu edad: ");
+                cliente.Edad = int.Parse(Console.ReadLine());
+                Console.Write("Ingresa tu dirección: ");
+                cliente.Direccion = Console.ReadLine();
+                Console.Clear();
+                if (cliente.Edad >= 18)
+                {
+                    Console.Clear();
+                    Console.Write("Ingresa el total de compra: ");
+                    double totalCompra = int.Parse(Console.ReadLine());
+                    Console.Clear();
+                    Console.WriteLine("--------------Datos del Cliente.-------------------");
+                    Console.WriteLine($"Cliente: {cliente.Nombre} {cliente.Apellido}");
+                    Console.WriteLine($"Edad: {cliente.Edad}");
+                    Console.WriteLine($"Dirección: {cliente.Direccion}");
+                    Console.WriteLine("---------------------------------");
+                    return funciones.CalcularDescuento(totalCompra);
+                }
+                else
+                {
+                    Console.WriteLine("Debes ser mayor de 18 años para realizar una compra.");
+                    return 0;
+                }
             }
-            else
+            catch (FormatException)
             {
-                Console.WriteLine("Debes ser mayor de 18 años para realizar una compra.");
+                Console.WriteLine("Error: El valor ingresado no es válido.");
+                return 0;
             }
-            Console.WriteLine("--------------Total de Compra.-------------------");
-            Console.WriteLine("Gracias por utilizar el programa. ¡Hasta luego!");
-            Console.ReadLine();
+            finally
+            {
+                Console.WriteLine("--------------Total de Compra.-------------------");
+                Console.WriteLine("Gracias por utilizar el programa. ¡Hasta luego!");
+                Console.ReadLine();
+            }
+            //public void MostrarMenu()
+            //{
+            //    Console.WriteLine("Bienvenido al programa de compras");
+            //    Console.WriteLine("---------------------------------");
+            //    Console.Write("Ingresa tu nombre: ");
+            //    cliente.Nombre = Console.ReadLine();
+            //    Console.Write("Ingresa tu apellido: ");
+            //    cliente.Apellido = Console.ReadLine();
+            //    Console.Write("Ingresa tu edad: ");
+            //    cliente.Edad = int.Parse(Console.ReadLine());
+            //    Console.Write("Ingresa tu dirección: ");
+            //    cliente.Direccion = Console.ReadLine();
+            //    Console.Clear();
+            //    if (cliente.Edad >= 18)
+            //    {
+            //        Console.Clear();
+            //        Console.Write("Ingresa el total de compra: ");
+            //        double totalCompra = int.Parse(Console.ReadLine());
+            //        Console.Clear();
+            //        Console.WriteLine("--------------Datos del Cliente.-------------------");
+            //        Console.WriteLine($"Cliente: {cliente.Nombre} {cliente.Apellido}");
+            //        Console.WriteLine($"Edad: {cliente.Edad}");
+            //        Console.WriteLine($"Dirección: {cliente.Direccion}");
+            //        Console.WriteLine("---------------------------------");
+            //        funciones.CalcularDescuento(totalCompra);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Debes ser mayor de 18 años para realizar una compra.");
+            //    }
+            //    Console.WriteLine("--------------Total de Compra.-------------------");
+            //    Console.WriteLine("Gracias por utilizar el programa. ¡Hasta luego!");
+            //    Console.ReadLine();
+            //}
         }
     }
 }
--- Aplicacion de tienda 2.0.2/Services/Funciones.cs
+++ Aplicacion de tienda 2.0.2/Services/Funciones.cs
@@ -8,48 +8,105 @@ namespace Aplicacion_de_tienda_2._0._2.Services
 {
     public class Funciones
     {
-        public void CalcularDescuento(double totalCompra)
-        {   double descuento = 0;
+
+        public double CalcularDescuento(double totalCompra)
+        {
+            double descuento = 0;
             string mesesSinIntereses = "";
 
-            if (totalCompra >= 10000)
+            try
             {
-                descuento = totalCompra * 0.3;
-                Console.WriteLine("¡Felicidades! Obtuviste un descuento del 30%.");
-                Console.WriteLine("Selecciona la cantidad de meses sin intereses:");
-                Console.WriteLine("1. 3 meses");
-                Console.WriteLine("2. 6 meses");
-                Console.Write("Opción: ");
-                int opcion = int.Parse(Console.ReadLine());
-                switch (opcion)
+                if (totalCompra >= 10000)
                 {
-                    case 1:
-                        Console.WriteLine("Has seleccionado 3 meses sin intereses y obtenido el descuento del 30%.");
-                        break;
-                    case 2:
-                        Console.WriteLine("Has seleccionado 6 meses sin intereses y obtenido el descuento del 30%.");
-                        break;
-                    default:
-                        Console.WriteLine("Opción inválida.");
-                        break;
+                    descuento = totalCompra * 0.3;
+                    Console.WriteLine("¡Felicidades! Obtuviste un descuento del 30%.");
+                    Console.WriteLine("Selecciona la cantidad de meses sin intereses:");
+                    Console.WriteLine("1. 3 meses");
+                    Console.WriteLine("2. 6 meses");
+                    Console.Write("Opción: ");
+                    int opcion = int.Parse(Console.ReadLine());
+                    switch (opcion)
+                    {
+                        case 1:
+                            Console.WriteLine("Has seleccionado 3 meses sin intereses y obtenido el descuento del 30%.");
+                            break;
+                        case 2:
+                            Console.WriteLine("Has seleccionado 6 meses sin intereses y obtenido el descuento del 30%.");
+                            break;
+                        default:
+                            Console.WriteLine("Opción inválida.");
+                            break;
+                    }
                 }
+                else if (totalCompra >= 5000)
+                {
+                    descuento = totalCompra * 0.2;
+                    Console.WriteLine("¡Enhorabuena! Obtuviste un descuento del 20% y 3 meses sin intereses.");
+                }
+                else
+                {
+                    Console.WriteLine("No aplican descuentos para esta compra.");
+                }
+                if (descuento > 0)
+                {
+                    Console.WriteLine("--------------Datos de Compra.--------------------");
+                    double montoConDescuento = totalCompra - descuento;
+                    Console.WriteLine($"Monto ingresado: {totalCompra:C}");
+                    Console.WriteLine($"Descuento obtenido: {descuento:C}");
+                    Console.WriteLine($"Monto con descuento: {montoConDescuento:C}");
+                }
+
+                return descuento;
             }
-            else if (totalCompra >= 5000)
+            catch (FormatException)
             {
-                descuento = totalCompra * 0.2;
-                Console.WriteLine("¡Enhorabuena! Obtuviste un descuento del 20% y 3 meses sin intereses.");
+                Console.WriteLine("Error: El valor ingresado no es válido.");
+                return 0;
             }
-            else
             {
-                Console.WriteLine("No aplican descuentos para esta compra.");
-            }
-            if (descuento > 0)
-            {
-                Console.WriteLine("--------------Datos de Compra.--------------------");
-                double montoConDescuento = totalCompra - descuento;
-                Console.WriteLine($"Monto ingresado: {totalCompra:C}");
-                Console.WriteLine($"Descuento obtenido: {descuento:C}");
-                Console.WriteLine($"Monto con descuento: {montoConDescuento:C}");
+                //public void CalcularDescuento(double totalCompra)
+                //{   double descuento = 0;
+                //    string mesesSinIntereses = "";
+
+                //    if (totalCompra >= 10000)
+                //    {
+                //        descuento = totalCompra * 0.3;
+                //        Console.WriteLine("¡Felicidades! Obtuviste un descuento del 30%.");
+                //        Console.WriteLine("Selecciona la cantidad de meses sin intereses:");
+                //        Console.WriteLine("1. 3 meses");
+                //        Console.WriteLine("2. 6 meses");
+                //        Console.Write("Opción: ");
+                //        int opcion = int.Parse(Console.ReadLine());
+                //        switch (opcion)
+                //        {
+                //            case 1:
+                //                Console.WriteLine("Has seleccionado 3 meses sin intereses y obtenido el descuento del 30%.");
+                //                break;
+                //            case 2:
+                //                Console.WriteLine("Has seleccionado 6 meses sin intereses y obtenido el descuento del 30%.");
+                //                break;
+                //            default:
+                //                Console.WriteLine("Opción inválida.");
+                //                break;
+                //        }
+                //    }
+                //    else if (totalCompra >= 5000)
+                //    {
+                //        descuento = totalCompra * 0.2;
+                //        Console.WriteLine("¡Enhorabuena! Obtuviste un descuento del 20% y 3 meses sin intereses.");
+                //    }
+                //    else
+                //    {
+                //        Console.WriteLine("No aplican descuentos para esta compra.");
+                //    }
+                //    if (descuento > 0)
+                //    {
+                //        Console.WriteLine("--------------Datos de Compra.--------------------");
+                //        double montoConDescuento = totalCompra - descuento;
+                //        Console.WriteLine($"Monto ingresado: {totalCompra:C}");
+                //        Console.WriteLine($"Descuento obtenido: {descuento:C}");
+                //        Console.WriteLine($"Monto con descuento: {montoConDescuento:C}");
+                //    }
             }
         }
     }
